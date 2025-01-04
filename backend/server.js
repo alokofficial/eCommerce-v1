13dd988b3909d6007f140eb6535e70f55da2d810
Alok Kumar <aloknh31@gmail.com>
2025-01-04 22:20:36 +0530

    asyncHandler and errorHandler middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,21 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
 
 const port = process.env.PORT || 5000;
 connectDB(); // connect to database
 const app = express();
 
+app.use('/api/products',productRoutes);
 app.get('/', (req, res) => {
     console.log("api is running")
     res.send('Hello World!')
 })
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    try {
-        const product = products.find(x => x._id === req.params.id)
-        if(!product) throw new Error('Product not found')
-        res.json(product)
-    } catch (error) {
-       res.status(404).send(error.message)
-    }
-})
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
