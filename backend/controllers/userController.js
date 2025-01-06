467ac6eb0a70817715fbc90febb3518c14799124
Alok Kumar <aloknh31@gmail.com>
2025-01-06 10:20:00 +0530

    added UserRoutes and userController and tested routers in postman

---
+++ backend/controllers/userController.js
@@ -0,0 +1,100 @@
+import asyncHadler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
+
+
+// @desc    Auth user and get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHadler(async(req, res) => {
+    res.send('auth user');
+})
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHadler(async(req, res) => {
+    res.send('register user');
+    // const {name, email, password} = req.body;
+    // const userExists = await User.findOne({email});
+    // if (userExists) {
+    //     res.status(400);
+    //     throw new Error('User already exists');
+    // }
+    // const user = await User.create({
+    //     name,
+    //     email,
+    //     password
+    // });
+    // if (user) {
+    //     res.status(201).json({
+    //         _id: user._id,
+    //         name: user.name,
+    //         email: user.email,
+    //         isAdmin: user.isAdmin,
+    //         token: generateToken(user._id)
+    //     })
+    // } else {    
+    //     res.status(400);
+    //     throw new Error('Invalid user data');
+    // }
+})
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Private
+const logoutUser = asyncHadler(async(req, res) => {
+    res.send('logout user');
+})
+
+// @desc    Get user Profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHadler(async(req, res) => {
+    res.send('get user profile');
+})
+
+// @desc    Update user Profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHadler(async(req, res) => {
+    res.send('update user profile');
+})
+
+// @desc    Get users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHadler(async(req, res) => {
+    res.send('get users');
+})
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUsers = asyncHadler(async(req, res) => {
+    res.send('delete users');
+})
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHadler(async(req, res) => {
+    res.send('get users by id');
+})
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUsers = asyncHadler(async(req, res) => {
+    res.send('update  users');
+})
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUsers,
+    getUserById,
+    updateUsers
+}
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ dotenv.config()
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 const port = process.env.PORT || 5000;
 connectDB(); // connect to database
 const app = express();
 
 app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
 app.get('/', (req, res) => {
     console.log("api is running")
     res.send('Hello World!')
