b105a4f52f3ca153082f2eb2b2de6b2079ad0d95
Alok Kumar <aloknh31@gmail.com>
2025-01-06 08:27:34 +0530

    added the functionality of item count in cart and showing on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
-import { LinkContainer } from 'react-router-bootstrap';
+// import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const { cartItems } = useSelector(state => state.cart);
+  // console.log(cartItems);
+
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-         <Container>
+         <Container> 
+          {/* need to use LinkContainer in order to use react router */}
             <Navbar.Brand href="/">
                 <img src={logo} alt="ProShop" />
                     ProShop
@@ -16,7 +21,8 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link href="/cart"> <FaShoppingCart /> Cart</Nav.Link>
+                <Nav.Link href="/cart"> <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && <Badge pill bg='success' style={{marginLeft: "5px"}} className="badge">{cartItems.reduce((acc, curr) => acc + curr.qty, 0)}</Badge>}</Nav.Link>
                 <Nav.Link href="/login"> <FaUser /> Sign In</Nav.Link>
               </Nav>
             </Navbar.Collapse>
