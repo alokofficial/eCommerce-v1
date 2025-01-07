6c90f1117da1062efccaa12ab9f8d7be4f81e955
Alok Kumar <aloknh31@gmail.com>
2025-01-08 04:23:34 +0530

    integrate PayPal button for UI

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -43,6 +43,40 @@ const OrderScreen = () => {
       }
     },[order, errorPayPal, loadingPayPal, paypal, payPalDispatch]);
 
+    function onApprove(data, actions) {
+      return actions.order.capture().then(async function (details) {
+        try {
+          await payOrder({orderId, details});
+          refetch();
+          toast.success('Payment successful');
+        } catch (err) {
+          toast.error(err?.data?.message || err.error);
+        }
+      })
+    }
+    async function onApproveTest(){
+      await payOrder({orderId, details:{payer:{}}});
+          refetch();
+          toast.success('Payment successful');
+    }
+    function onError(err) {
+      toast.error(err.message);
+    }
+    function createOrder(data, actions) {
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: order.totalPrice,
+            },
+          },
+        ],
+      }).then((orderId) => {
+        
+        return orderId;
+      });
+    }
+
   return isLoading ? (
     <Loader />
   ) : error ? (
@@ -147,7 +181,27 @@ const OrderScreen = () => {
                   <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-              {/* PAY ORDER PLACEHOLDER */}
+              {!order.isPaid && (
+                <ListGroup.Item>
+                  {loadingPay && <Loader />}
+                  {isPending ? (
+                    <Loader />
+                  ) : (
+                    <div>
+                      <Button onClick={onApproveTest} style={{marginBottom: '10px'}}>
+                        Test Pay Order 
+                      </Button>
+                      <div>
+                        <PayPalButtons
+                          createOrder={createOrder}
+                          onApprove={onApprove}
+                          onError={onError}
+                        />
+                      </div>
+                    </div>
+                  )}
+                </ListGroup.Item>
+              )}
               {/* MARK AS DELIVERED PLACEHOLDER */}
             </ListGroup>
           </Card>
