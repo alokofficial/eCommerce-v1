26b6b3cc8836ef3da2d07261534595ac8a421c67
Alok Kumar <aloknh31@gmail.com>
2025-01-09 20:03:17 +0530

    before deployement push

--- backend/server.js
+++ backend/server.js
@@ -27,12 +27,22 @@ app.use('/api/upload',uploadRoutes);
 app.get('/api/config/paypal', (req, res) => res.send({clientId:process.env.PAYPAL_CLIENT_ID})
 )
 
-app.get('/', (req, res) => {
-    console.log("api is running")
-    res.send('Hello World!')
-})
 const __dirname = path.resolve(); //Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    // Any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}else{
+    app.get('/', (req, res) => {
+        res.send('API is running..');
+    });
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
